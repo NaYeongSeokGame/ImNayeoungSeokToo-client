a4da47fe64e219877cea421aca71099ea55a4dae
Ju-ae <qowndo@gmail.com>
2023-06-25 00:38:32 +0900

    [feat] 퀴즈 갯수 카운터 추가

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CreateQuiz = () => {
+  const [quizCount, setQuizCount] = useState(9);
+  const createButton =()=>{
+  }
+
+  const onPlusClicked=()=>{
+    setQuizCount(quizCount +1);
+  }
+  const onMinusClicked=()=>{
+    if(quizCount > 1){
+      setQuizCount(quizCount-1);
+    }else{
+      alert("1개 이상 문제를 만들어주세요. ");
+    }
+  }
+
   return (
     <div>
-      
+      <button onClick={onMinusClicked}>-</button>    
+      {quizCount}
+      <button onClick={onPlusClicked}>+</button>    
+
+      <div>
+      </div>
     </div>
   );
 };
