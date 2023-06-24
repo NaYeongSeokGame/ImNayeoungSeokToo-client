5f2b5aa1a80e5511203e3f5b9998ab8e5dfa1dd0
jiyoung <cwd05011@gmail.com>
2023-06-25 03:25:23 +0900

    feat: 이미지 업로드, 미리보기 및 정답 제출 구현

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useState } from 'react';
+
+import UploadModal from '@/components/UploadModal';
+import { QuizFileAndAnswer } from '@/types/imageUploadmodal';
 
 const CreateQuiz = () => {
+  const [quizPreset, setQuizPreset] = useState<QuizFileAndAnswer[]>([]);
   return (
     <div>
-      
+      <UploadModal setQuizPreset={setQuizPreset} />
     </div>
   );
 };
