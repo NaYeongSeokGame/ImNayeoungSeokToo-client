76f85da4eb51a28134a9529a94a8811a43e85232
Shinpanda <duldulgo1@gmail.com>
2023-06-25 18:10:45 +0900

    [fix] Modal로 변경되면서 pages 변경이 없다 생각되어 이전 상태로 revert

--- src/pages/PrepareQuiz.tsx
+++ src/pages/PrepareQuiz.tsx
@@ -1,23 +1,3 @@
-import { useContext } from 'react';
-
-import Modal from '@/components/Modal';
-import QuizPreset from '@/components/QuizPreset';
-import useModal from '@/hooks/useModal';
-import { QuizStateContext } from '@/utils/QuizContext';
-
 const PrepareQuiz = () => {
-  const { isOpen, onClose, onOpen } = useModal();
-  const { timer } = useContext(QuizStateContext);
-
-  return (
-    <>
-      <div>timer: {timer}</div>
-      <button onClick={onOpen}>click</button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <QuizPreset title={'title'} imgUrl="" />
-      </Modal>
-    </>
-  );
+  return <div></div>;
 };
-
-export default PrepareQuiz;
