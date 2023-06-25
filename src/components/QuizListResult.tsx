ee395068fa0f681c7047c7263dc85f8fe7911f25
kknyapple <kknyapple@gmail.com>
2023-06-25 22:04:37 +0900

    feat: 홈 화면으로 이동하는 버튼 구현

--- src/components/QuizListResult.tsx
+++ src/components/QuizListResult.tsx
@@ -35,6 +35,13 @@ const Button = styled.button`
   font-weight: 700;
   font-size: 20px;
 `;
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 120px;
+  width: 300px;
+`;
 const QuizResult = ({
   score,
   totalQuiz,
@@ -44,6 +51,9 @@ const QuizResult = ({
     <ScoreContainer>
       <CorrectScore>{score}</CorrectScore>/{totalQuiz}
     </ScoreContainer>
+    <ButtonContainer>
+      <Button onClick={handleHomeButtonClick}>돌아가기</Button>
+    </ButtonContainer>
   </Container>
 );
 
