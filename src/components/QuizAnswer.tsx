0997d0fdfa5fcb313fc3e003e062ef395708dc81
kknyapple <kknyapple@gmail.com>
2023-06-25 21:58:04 +0900

    feat: O, X 버튼 클릭 시 다음 문제 화면 이동 및 점수 반영

--- src/components/QuizAnswer.tsx
+++ src/components/QuizAnswer.tsx
@@ -7,7 +7,6 @@ const Container = styled.div`
   justify-content: center;
   height: 100vh;
 `;
-
 const AnswerText = styled.div`
   border: 3px solid black;
   width: 300px;
@@ -18,7 +17,20 @@ const AnswerText = styled.div`
   align-items: center;
   justify-content: center;
 `;
-
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 120px;
+  width: 300px;
+`;
+const Button = styled.button`
+  height: 70px;
+  width: 70px;
+  background-color: white;
+  border: 3px solid black;
+  border-radius: 8px;
+`;
 const QuizAnswer = ({
   index,
   setIndex,
@@ -39,10 +51,24 @@ const QuizAnswer = ({
     setIsAnswerButtonClicked(false);
   };
 
+  const handleGoBack = (): void => {
+    setIsMomentAnswerButtonClicked(false);
+  };
+
   return (
     <Container>
       <h1>문제 {index + 1}</h1>
       <AnswerText>{currentQuiz.answer}</AnswerText>
+      <ButtonContainer>
+        {isMomentAnswerButtonClicked ? (
+          <Button>뒤로가기</Button>
+        ) : (
+          <>
+            <Button onClick={() => handleAnswer(true)}>O</Button>
+            <Button onClick={() => handleAnswer(false)}>X</Button>
+          </>
+        )}
+      </ButtonContainer>
       <div>{score /*삭제 예정*/}</div>
     </Container>
   );
