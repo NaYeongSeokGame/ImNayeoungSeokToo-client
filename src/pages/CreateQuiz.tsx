49af77cf173c76e0fe4858bfe102014eef966b21
jiyoung <cwd05011@gmail.com>
2023-06-25 11:43:21 +0900

    test: eslint 룰적용으로 quizPreset 임시 사용

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -5,8 +5,16 @@ import { QuizFileAndAnswer } from '@/types/imageUploadmodal';
 
 const CreateQuiz = () => {
   const [quizPreset, setQuizPreset] = useState<QuizFileAndAnswer[]>([]);
+
+  // eslint 룰적용으로 quizPreset 임시 사용
+  const preset = () =>
+    quizPreset.map((quiz, idx) => (
+      <div key={`${idx}-${quiz.answer}`}>{quiz.answer}</div>
+    ));
+
   return (
     <div>
+      <>{preset}</>
       <UploadModal setQuizPreset={setQuizPreset} />
     </div>
   );
