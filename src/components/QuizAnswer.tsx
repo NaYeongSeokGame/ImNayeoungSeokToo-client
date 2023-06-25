7d05723070ee75452d1229066396ba307c3c4e8f
kknyapple <kknyapple@gmail.com>
2023-06-26 00:26:45 +0900

    style: UI 수정

--- src/components/QuizAnswer.tsx
+++ src/components/QuizAnswer.tsx
@@ -8,34 +8,57 @@ const Container = styled.div`
   height: 100vh;
 `;
 const AnswerText = styled.div`
-  border: 3px solid black;
-  width: 300px;
-  height: 360px;
-  border-radius: 8px;
-
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 18.75rem;
+  height: 23rem;
+  flex-shrink: 0;
+  border-radius: 20px;
+  border: 5px solid #000;
+  background: #fff;
+  box-shadow: 1px 4px 2px 0px rgba(0, 0, 0, 0.1);
+
+  color: #000;
+  font-size: 5rem;
+  font-family: Pretendard;
+  font-weight: 700;
 `;
 const ButtonContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  height: 120px;
-  width: 300px;
 `;
 const Button = styled.button`
-  height: 70px;
-  width: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1.25rem;
+  margin-left: 4.375rem;
+  margin-right: 4.375rem;
+  width: 5rem;
+  height: 5rem;
+  flex-shrink: 0;
   background-color: white;
-  border: 3px solid black;
-  border-radius: 8px;
+  border: 5px solid #000;
+  border-radius: 20px;
+`;
+
+const GoBackButton = styled.button`
+  width: 13.75rem;
+  height: 4rem;
+  flex-shrink: 0;
+  color: #7443ff;
+  background-color: white;
+  border: 5px solid black;
+  border-radius: 20px;
+  font-weight: 700;
+  font-size: 1.125rem;
+  margin-top: 1.25rem;
 `;
 const QuizAnswer = ({
   index,
   setIndex,
   setIsAnswerButtonClicked,
-  score,
   setScore,
   preset,
   isMomentAnswerButtonClicked,
@@ -61,15 +84,54 @@ const QuizAnswer = ({
       <AnswerText>{currentQuiz.answer}</AnswerText>
       <ButtonContainer>
         {isMomentAnswerButtonClicked ? (
-          <Button onClick={handleGoBack}>뒤로가기</Button>
+          <GoBackButton onClick={handleGoBack}>뒤로가기</GoBackButton>
         ) : (
           <>
-            <Button onClick={() => handleAnswer(true)}>O</Button>
-            <Button onClick={() => handleAnswer(false)}>X</Button>
+            <Button onClick={() => handleAnswer(true)}>
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="16"
+                  cy="16"
+                  r="11"
+                  stroke="#7443FF"
+                  stroke-width="10"
+                />
+              </svg>
+            </Button>
+            <Button onClick={() => handleAnswer(false)}>
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="23.2727"
+                  width="12.3422"
+                  height="32.9126"
+                  transform="rotate(45 23.2727 0)"
+                  fill="#FF0000"
+                />
+                <rect
+                  x="7.62939e-06"
+                  y="8.72726"
+                  width="12.3422"
+                  height="32.9126"
+                  transform="rotate(-45 7.62939e-06 8.72726)"
+                  fill="#FF0000"
+                />
+              </svg>
+            </Button>
           </>
         )}
       </ButtonContainer>
-      <div>{score /*삭제 예정*/}</div>
     </Container>
   );
 };
