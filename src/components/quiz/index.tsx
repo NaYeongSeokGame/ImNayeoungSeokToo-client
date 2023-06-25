c5fd12236aca4e960aeab8d763418f1acaed0d6e
Forourhappy <foroh1504@gmail.com>
2023-06-26 00:29:16 +0900

    feat: 이미지 컴포넌트 제거

--- src/components/quiz/index.tsx
+++ src/components/quiz/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-import QuizImg from '@/components/quiz/QuizImg.tsx';
 import { Quiz } from '@/types/Quiz.ts';
 
 const QuizContent = ({
@@ -23,13 +23,13 @@ const QuizContent = ({
   }, [currentIdx, isLastIdx, isNext, isTimeout, setGameEnd]);
 
   return (
-    <>
+    <QuizContentWrapper>
       {isTimeout || isShowAnswer ? (
-        <span>{quizList[currentIdx].answer}</span>
+        <QuizText>{quizList[currentIdx].answer}</QuizText>
       ) : (
-        <QuizImg imageUrl={quizList[currentIdx].imageUrl} />
+        <QuizImg url={quizList[currentIdx].imageUrl} />
       )}
-    </>
+    </QuizContentWrapper>
   );
 };
 
@@ -42,3 +42,29 @@ type QuizProps = {
   isShowAnswer: boolean;
   setGameEnd: (isEnd: boolean) => void;
 };
+
+const QuizContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 18.75rem;
+  height: 23rem;
+  border-radius: 20px;
+  border: 5px;
+  border: 5px solid #000000;
+  box-shadow: 1px 4px 2px 0px #0000001a;
+`;
+
+const QuizText = styled.div`
+  color: #000;
+  font-size: 5rem;
+  font-family: Pretendard;
+  font-weight: 700;
+`;
+
+const QuizImg = styled.div<{ url: string }>`
+  width: 100%;
+  margin: 0.875rem 0.625rem;
+  background: ${(props) => `url(${props.url}) center/contain no-repeat`};
+  border-radius: 20px;
+`;
