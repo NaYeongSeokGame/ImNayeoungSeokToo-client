cd91abc502aa6976c6afcceb45fae3e485c6cc7b
Shinpanda <duldulgo1@gmail.com>
2023-06-25 20:41:44 +0900

    [refactor] 필요없는 속성 제거

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -7,22 +7,22 @@ export const DefaultButtonStyle = css`
   cursor: pointer;
 `;
 
-export const SmallText1 = styled.span<React.HTMLAttributes<HTMLSpanElement>>`
+export const SmallText1 = styled.span`
   font-size: 16px;
 `;
 
-export const SmallText2 = styled.span<React.HTMLAttributes<HTMLSpanElement>>`
+export const SmallText2 = styled.span`
   font-size: 18px;
 `;
 
-export const MediumText = styled.span<React.HTMLAttributes<HTMLSpanElement>>`
+export const MediumText = styled.span`
   font-size: 32px;
 `;
 
-export const TitleText = styled.span<React.HTMLAttributes<HTMLSpanElement>>`
+export const TitleText = styled.span`
   font-size: 96px;
 `;
 
-export const QCountText = styled.span<React.HTMLAttributes<HTMLSpanElement>>`
+export const QCountText = styled.span`
   font-size: 42px;
 `;
