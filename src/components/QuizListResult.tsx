a98066dc314552593ee30dc70200f732d52725f4
kknyapple <kknyapple@gmail.com>
2023-06-25 22:03:54 +0900

    feat: 결과 화면 보여주기

---
+++ src/components/QuizListResult.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const ScoreContainer = styled.div`
+  border: 3px solid black;
+  width: 300px;
+  height: 360px;
+  border-radius: 8px;
+  font-size: 50px;
+  font-weight: 700;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CorrectScore = styled.div`
+  color: #7443ff;
+`;
+
+const Button = styled.button`
+  height: 70px;
+  width: 200px;
+  color: #7443ff;
+  background-color: white;
+  border: 3px solid black;
+  border-radius: 8px;
+  font-weight: 700;
+  font-size: 20px;
+`;
+const QuizResult = ({
+  score,
+  totalQuiz,
+  handleHomeButtonClick,
+}: QuizListResultProps) => (
+  <Container>
+    <ScoreContainer>
+      <CorrectScore>{score}</CorrectScore>/{totalQuiz}
+    </ScoreContainer>
+  </Container>
+);
+
+export default QuizResult;
