e41f05de77a097b519b357d265869f61c6e6ec2c
Ju-ae <qowndo@gmail.com>
2023-06-25 16:57:48 +0900

    feat: 문제 갯수 3~9개로 제한

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -14,6 +14,7 @@ const CreateQuiz = () => {
   useEffect(() => {
     renderQuizButton(count);
   }, []);
+
   const renderQuizButton = (count: number) => {
     setCount(count); //데이터 입력 중에 추가했을 때는?
     const temp = [];
@@ -28,13 +29,17 @@ const CreateQuiz = () => {
   };
 
   const onPlusClicked = () => {
+    if(count >8){
+      alert("문제는 최대 9개까지 생성할 수 있습니다");
+    }
     renderQuizButton(count + 1);
   };
+
   const onMinusClicked = () => {
-    if (count > 1) {
+    if (count > 4) {
       renderQuizButton(count - 1);
     } else {
-      alert('1개 이상 문제를 만들어주세요. ');
+      alert('3개 이상 문제를 만들어주세요. ');
     }
   };
   const addContent = (event: React.MouseEvent<HTMLElement>) => {
@@ -46,44 +51,48 @@ const CreateQuiz = () => {
     //api에 업로드
   };
   return (
-      <CreateQuizLayout>
-        <QuizTitleLayout>
-
+    <CreateQuizLayout>
+      <QuizTitleLayout>
         <QuizTitleInput
           type="text"
           placeholder="퀴즈 이름을 입력해주세요 "
           onChange={handleInputText}
           value={title}
         ></QuizTitleInput>
-        </QuizTitleLayout>
-        <QuizCounter>
-          <button onClick={onMinusClicked}>-</button>
-          {count}
-          <button onClick={onPlusClicked}>+</button>
-        </QuizCounter>
-        <QuizGrid></QuizGrid>
+      </QuizTitleLayout>
+      <QuizCounter>
+        <button onClick={onMinusClicked}>-</button>
+        {count}
+        <button onClick={onPlusClicked}>+</button>
+      </QuizCounter>
 
-        <AddQuizButtonGrid
-          fileAndAnswer={fileAndAnswer}
-          addContent={addContent}
-        />
-        <button>홈 화면으로 돌아가기</button>
-      </CreateQuizLayout>
+
+      <AddQuizButtonGrid
+        fileAndAnswer={fileAndAnswer}
+        addContent={addContent}
+      />
+      <button>홈 화면으로 돌아가기</button>
+    </CreateQuizLayout>
   );
 };
 
 export default CreateQuiz;
 
 const CreateQuizLayout = styled.div`
-  margin: 0 auto;
+margin : 0 auto;
+  padding: 50px 0px;
   width: 30vw;
   height: 100vh;
   background-color: grey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 const QuizTitleInput = styled.input``;
-const QuizTitleLayout = styled.div`margin: 0 auto`;
+const QuizTitleLayout = styled.div`
+  margin: 0 auto;
+`;
 
 const QuizCounter = styled.div``;
-
-const QuizGrid = styled.div``;
