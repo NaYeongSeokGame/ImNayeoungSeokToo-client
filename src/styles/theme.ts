42a2eb040f44375d64ccd68370be9c01691668a6
지영 <108715216+ohtmm@users.noreply.github.com>
2023-06-25 22:39:47 +0900

    [feat] 공통 레이아웃 스타일 및 헤더 구현 (#14)

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  color: {
+    primaryPurple: '#7443FF',
+    primaryRed: '#FF0000',
+    black: '#000000',
+  },
+  size: {},
+};
