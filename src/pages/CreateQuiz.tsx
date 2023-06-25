b743362e1f26aa529e92f6884b1c044c1d616f5e
지영 <108715216+ohtmm@users.noreply.github.com>
2023-06-26 00:20:57 +0900

    [feat] 이미지 업로드, 미리보기 및 정답 제출 구현  #8 (#13)
    Co-authored-by: Gwangin Baik <gwangin1999@naver.com>

--- src/pages/CreateQuiz.tsx
+++ src/pages/CreateQuiz.tsx
@@ -4,10 +4,17 @@ import UploadModal from '@/components/UploadModal';
 import { QuizFileAndAnswer } from '@/types/imageUploadmodal';
 
 const CreateQuiz = () => {
-  const [, setQuizPreset] = useState<QuizFileAndAnswer[]>([]);
+  const [quizPreset, setQuizPreset] = useState<QuizFileAndAnswer[]>([]);
+
+  // eslint 룰적용으로 quizPreset 임시 사용
+  const preset = () =>
+    quizPreset.map((quiz, idx) => (
+      <div key={`${idx}-${quiz.answer}`}>{quiz.answer}</div>
+    ));
+
   return (
     <div>
-      <h1>create Quiz</h1>
+      <>{preset}</>
       <UploadModal setQuizPreset={setQuizPreset} />
     </div>
   );
