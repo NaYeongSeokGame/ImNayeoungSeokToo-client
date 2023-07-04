13e70e3eb64b3bfa2fc6d47fa4a3b6cffb2debb2
RookieAND <gwangin1999@naver.com>
2023-07-05 00:51:41 +0900

    refactor: 항상 최신의 timerId를 바라보도록 useTimer 개선

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,26 +1,29 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
-function useTimer(initSecond = 0) {
-  const [currentSecond, setCurrentSecond] = useState(initSecond);
-  const [isRunning, setIsRunning] = useState(false);
+function useTimer({ initTimeLimit = 0, startImmediately = false }) {
+  const [leftSecond, setLeftSecond] = useState(initTimeLimit);
+  const [isRunning, setIsRunning] = useState(startImmediately);
+
+  // NOTE : 100ms 간격으로 시간을 줄이는 TimerId Ref, 매 랜더링마다 최신의 Id를 바라보도록 함.
+  const timerRef = useRef<NodeJS.Timer | undefined>(undefined);
 
   const start = useCallback(() => setIsRunning(true), []);
   const stop = useCallback(() => setIsRunning(false), []);
 
   useEffect(() => {
-    let timerId: NodeJS.Timer | undefined;
+    timerRef.current = setInterval(() => {
+      setLeftSecond((prev) => prev - 1);
+    }, 1000);
 
-    if (isRunning) {
-      timerId = setInterval(() => setCurrentSecond((prev) => prev + 1), 1000);
-      return;
+    if (!isRunning || leftSecond <= 0) {
+      clearInterval(timerRef.current);
+      timerRef.current = undefined;
     }
 
-    if (timerId) clearInterval(timerId);
+    return () => clearInterval(timerRef.current);
+  }, [isRunning, leftSecond]);
 
-    return () => clearInterval(timerId);
-  }, [isRunning]);
-
-  return { currentSecond, start, stop };
+  return { leftSecond, isRunning, start, stop };
 }
 
 export default useTimer;
