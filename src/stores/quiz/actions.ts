05266382604316e852e1d0e4638e288492564639
Baik Gwangin <gwangin1999@naver.com>
2023-07-04 17:50:14 +0900

    feat: 게임 진행 상태 여부에 따라 에러를 throw 하는 로직 추가

--- src/stores/quiz/actions.ts
+++ src/stores/quiz/actions.ts
@@ -10,6 +10,9 @@ export const controlPlayingStateAtom = atom(
   (get) => get(quizPlayStateAtom).isPlaying,
   (get, set, updatedPlayingState: boolean) => {
     const prevAtom = get(quizPlayStateAtom);
+    if (prevAtom.isPlaying == updatedPlayingState)
+      throw new Error('업데이트하려는 게임 상태가 동일합니다.')
+
     set(quizPlayStateAtom, { ...prevAtom, isPlaying: updatedPlayingState });
   },
 );
@@ -18,6 +21,10 @@ export const controlCurrentScoreAtom = atom(
   null,
   (get, set, update: UpdateCurrentScoreType) => {
     const prevAtom = get(quizPlayStateAtom);
+
+    if (!prevAtom.isPlaying)
+      throw new Error('게임이 시작하기 전에 스코어를 변경할 수 없습니다.')
+
     const totalScore = prevAtom.totalScore + Number(update.isCorrect);
     set(quizPlayStateAtom, {
       ...prevAtom,
@@ -31,6 +38,9 @@ export const initQuizPresetDataAtom = atom(
   null,
   (get, set, update: UpdateQuizPresetType) => {
     const prevAtom = get(quizPlayStateAtom);
+    if (prevAtom.isPlaying)
+      throw new Error('게임이 진행 중인 상황에서 프리셋을 업데이트 할 수 없습니다.')
+
     set(quizPlayStateAtom, { ...prevAtom, ...update });
   },
 );
@@ -38,6 +48,12 @@ export const initQuizPresetDataAtom = atom(
 export const resetQuizStateAtom = atom(
   null,
   (get, set, _) => {
+    const prevAtom = get(quizPlayStateAtom);
+
+    if (!prevAtom.isPlaying) {
+      throw new Error('아직 게임이 시작되지 않은 상태입니다.')
+    }
+
     set(quizPlayStateAtom, {
       isPlaying: false,
       presetPin: '',
