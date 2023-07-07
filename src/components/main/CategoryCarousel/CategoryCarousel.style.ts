827839c36b76d1d270fdb2cab4388942aa5c9860
Ju-ae <qowndo@gmail.com>
2023-07-07 15:09:08 +0900

    feat: 캐러셀 요소 디자인 변경

--- src/components/main/CategoryCarousel/CategoryCarousel.style.ts
+++ src/components/main/CategoryCarousel/CategoryCarousel.style.ts
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   width: 22.5rem;
   overflow-x: auto;
   padding-left: 0.5rem;
+  padding-top: 4rem;
 
   display: flex;
   justify-content: start;
@@ -22,32 +23,59 @@ export const Carousel = styled.div`
   gap: 0 0.5rem;
 `;
 
-export const Image = styled.div<{ imageUrl: string; isSelected: boolean }>`
-  ${({ theme, imageUrl, isSelected }) => {
-    return css`
-      width: 120px;
-      height: 120px;
 
-      background-image: url(${imageUrl});
-      background-size: cover;
-      border: 0.25rem solid ${theme.colors.black};
+export const ImageWrapper = styled.div`
+  ${({ theme }) => {
+    return css`
+      width: 128px;
+      height: 170px;
+
+      padding-top: 8px;
+      padding-left: 8px;
+      background-color: grey;
       border-radius: 1rem;
       box-shadow: 0px 0.25rem 0.25rem 0px rgba(0, 0, 0, 0.15);
 
       display: flex;
-      flex-direction: column-reverse;
+      flex-direction: column;
     `;
   }}
 `;
 
-export const CategoryText = styled.span`
+export const Image = styled.div<{ imageUrl: string; isSelected: boolean }>`
+  ${({ theme, imageUrl, isSelected }) => {
+    return css`
+      width: 110px;
+      height: 110px;
+
+      background-image: url(${imageUrl});
+      background-size: cover;
+      border-radius: 1rem;
+      box-shadow: 0px 0.25rem 0.25rem 0px rgba(0, 0, 0, 0.15);
+      
+    `;
+  }}
+`;
+
+export const TitleText = styled.span`
   ${({ theme }) => {
     return css`
       font-size: 1rem;
       color: ${theme.colors.black};
-      text-align: right;
+
+      margin: 0.5rem 0 ;
+    `;
+  }}
+`;
+
+export const HashtagText = styled.span`
+  ${({ theme }) => {
+    return css`
+      font-size: 0.5rem;
+      color: ${theme.colors.black};
 
       margin: 0 0.625rem 0.5rem 0;
     `;
   }}
 `;
+
--- src/components/main/CategoryCarousel/CategoryCarousel.tsx
+++ src/components/main/CategoryCarousel/CategoryCarousel.tsx
@@ -8,42 +8,38 @@ import getCloudFrontUrl from '@/utils/getCloudFrontUrl';
 import GameStartModal from '../GameStartModal';
 import * as styles from './CategoryCarousel.style';
 
-interface CategoryElementProps {
-  imageUrl: string;
-  category: string;
-  isSelected: boolean;
-  presetPin: string;
-}
-
-function getQuizListLocal(){ //Fix : 로컬 테스트용, 서버에 데이터 반영되면 삭제 예정
-  return [ {
-    thumbnailUrl:
-      'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499921cda2ab71457cff4f3/8c884b422460c98c.jpeg',
-    title: '스포츠',
-    isPrivate: false,
-    presetPin: '649991e4da2ab71457cff4e7',
-  },
-  {
-    imageUrl:
-      'http://d2e2zp9buqlqpp.cloudfront.net/preset/64999226da2ab71457cff4ff/이강인.jpg',
-    title: '축구선수',
-    isPrivate: false,
-    presetPin: '64999226da2ab71457cff4ff',
-  },
-  {
-    imageUrl:
-      'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499927bda2ab71457cff527/c39374225f7a80a0a9dcc2066eedc3de.jpg',
-    title: '남자연예인',
-    isPrivate: false,
-    presetPin: '6499927bda2ab71457cff527',
-  },
-  {
-    imageUrl:
-      'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499921cda2ab71457cff4f3/8c884b422460c98c.jpeg',
-    title: '여자연예인',
-    isPrivate: false,
-    presetPin: '649993fbda2ab71457cff55f',
-  }];
+function getQuizListLocal() {
+  //Fix : 로컬 테스트용, 서버에 데이터 반영되면 삭제 예정
+  return [
+    {
+      thumbnailUrl:
+        'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499921cda2ab71457cff4f3/8c884b422460c98c.jpeg',
+      title: '스포츠',
+      isPrivate: false,
+      presetPin: '649991e4da2ab71457cff4e7',
+    },
+    {
+      thumbnailUrl:
+        'http://d2e2zp9buqlqpp.cloudfront.net/preset/64999226da2ab71457cff4ff/이강인.jpg',
+      title: '축구선수',
+      isPrivate: false,
+      presetPin: '64999226da2ab71457cff4ff',
+    },
+    {
+      thumbnailUrl:
+        'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499927bda2ab71457cff527/c39374225f7a80a0a9dcc2066eedc3de.jpg',
+      title: '남자연예인',
+      isPrivate: false,
+      presetPin: '6499927bda2ab71457cff527',
+    },
+    {
+      thumbnailUrl:
+        'http://d2e2zp9buqlqpp.cloudfront.net/preset/6499921cda2ab71457cff4f3/8c884b422460c98c.jpeg',
+      title: '여자연예인',
+      isPrivate: false,
+      presetPin: '649993fbda2ab71457cff55f',
+    },
+  ];
 }
 
 const CategoryCarousel = () => {
@@ -63,8 +59,8 @@ const CategoryCarousel = () => {
         console.error(error);
       }
     };
-
-    getPresetList(); 
+    // getPresetList();
+    setPresetList(getQuizListLocal());
   }, []);
 
   const handleGoToQuizPreset = (
@@ -93,15 +89,17 @@ const CategoryCarousel = () => {
           }: QuizPresetType) => (
             console.log(title),
             (
-              <styles.Image
-                imageUrl={thumbnailUrl}
-                isSelected={false}
-                onClick={() =>
-                  handleGoToQuizPreset(presetPin, thumbnailUrl, title)
-                }
-              >
-                <styles.CategoryText>{title}</styles.CategoryText>
-              </styles.Image>
+              <styles.ImageWrapper>
+                <styles.Image
+                  imageUrl={thumbnailUrl}
+                  isSelected={false}
+                  onClick={() =>
+                    handleGoToQuizPreset(presetPin, thumbnailUrl, title)
+                  }
+                />
+                <styles.TitleText>{title}</styles.TitleText>
+                <styles.HashtagText>{title}</styles.HashtagText>
+              </styles.ImageWrapper>
             )
           ),
         )}
