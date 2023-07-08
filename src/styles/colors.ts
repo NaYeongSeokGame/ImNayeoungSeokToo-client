02e2bb963784c84746ffaa2b4557147d45c5ed6a
Bae Juae <qowndo@gmail.com>
2023-07-08 11:33:19 +0900

    [INT-38] 폰트 파일 및 폰트/컬러 변수값 추가 (#34)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,44 @@
 export const COLORS = {
+  // FIXME : purple, red, black, white 컬러는 UI 마이그레이션이 종료된 이후 제거 예정
   purple: '#7443FF',
   red: '#FF0000',
   black: '#000000',
   white: '#FFFFFF',
+
+  cremewhite: '#FCFBFA',
+  dark: '141514',
+
+  gray100: '#ECECEC',
+  gray200: '#CECFCD',
+  gray300: '#C8C8C8',
+  gray400: '#AAAAAA',
+  gray500: '#6E726E',
+  gray600: '#505050',
+  gray700: '#3B3D3B',
+  gray800: '#242424',
+
+
+  darkblue050: '#F1F0FF',
+  darkblue100: '#E7E4FF',
+  darkblue200: '#D1CDFF',
+  darkblue300: '#B1A5FF',
+  darkblue400: '#8B72FF',
+  darkblue500: '#673AFF',
+  darkblue600: '#5712FF',
+  darkblue700: '#4701FF',
+  darkblue800: '#3C00DB',
+  darkblue900: '#3202B0',
+  darkblue950: '#1A0078',
+
+  magenta050: '#FBF5FF',
+  magenta100: '#EFD3FF',
+  magenta200: '#EED2FE',
+  magenta300: '#E4B0FF',
+  magenta400: '#D37EFF',
+  magenta500: '#BB37FF',
+  magenta600: '#B12AF3',
+  magenta700: '#9B1AD6',
+  magenta800: '#821AAF',
+  magenta900: '#6A178C',
+  magenta950: '#4B0269',
 };
