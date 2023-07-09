77f4ffc31672cffffe0bf3f350b525e9f149f96b
Forourhappy <foroh1504@gmail.com>
2023-07-09 12:44:34 +0900

    rename: audioStateType, initAudioSetting 이름 변경

--- src/stores/audio/atoms.ts
+++ src/stores/audio/atoms.ts
@@ -1,9 +1,10 @@
 import { atom } from 'jotai';
 import { AudioStateType } from 'src/types/atom/audio';
 
+
 export const audioStateAtom = atom<AudioStateType>({
   backgroundSoundSrc: new Audio(),
   isMute: false,
-  backgroundSound: 1,
-  soundEffect: 1,
+  backgroundVolume: 1,
+  soundEffectVolume: 1,
 });
--- src/types/atom/audio/index.ts
+++ src/types/atom/audio/index.ts
@@ -4,14 +4,14 @@ export interface AudioStateType {
   /** 전체 사운드 음소거 여부 */
   isMute: boolean;
   /** 전체 게임 중 배경음 볼륨 */
-  backgroundSound: number;
+  backgroundVolume: number;
   /** 전체 게임 중 효과음 볼륨 */
-  soundEffect: number;
+  soundEffectVolume: number;
 }
 
 export type UpdateAudioStateType = Pick<
   AudioStateType,
-  'backgroundSound' | 'soundEffect' | 'isMute'
+  'backgroundVolume' | 'soundEffectVolume' | 'isMute'
 >;
 
 export type SettingAudioStateType = Pick<AudioStateType, 'backgroundSoundSrc'>;
