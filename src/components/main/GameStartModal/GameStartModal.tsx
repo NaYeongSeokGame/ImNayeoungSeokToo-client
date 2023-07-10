d66aa43ab153e350b57a95efa9f4af2ecb4a0d33
Foh <111723390+Forourhappy@users.noreply.github.com>
2023-07-10 21:27:26 +0900

    [INT-15] 문제 풀이 시간 설정 (#36)

--- src/components/main/GameStartModal/GameStartModal.tsx
+++ src/components/main/GameStartModal/GameStartModal.tsx
@@ -1,13 +1,11 @@
-import { useSetAtom } from 'jotai';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import QuizRepository from '@/apis/quiz';
 import { ReactComponent as MinusIconSvg } from '@/assets/icons/minusIcon.svg';
 import { ReactComponent as PlusIconSvg } from '@/assets/icons/plusIcon.svg';
 import ModalTemplate from '@/components/common/ModalTemplate';
 import useModal from '@/hooks/useModal';
-import { startQuizGameAtom } from '@/stores/quiz';
+import usePresetSetting from '@/hooks/usePresetSetting.ts';
 import getCloudFrontUrl from '@/utils/getCloudFrontUrl';
 
 import * as styles from './GameStartModal.style';
@@ -24,26 +22,16 @@ const GameStartModal = ({
   title,
 }: GameStartModalProps) => {
   const navigate = useNavigate();
-  const startQuizGame = useSetAtom(startQuizGameAtom);
   const { closeModal } = useModal();
 
-  const [delayBeforeStart, setDelayBeforeStart] = useState(3);
-
-  const handleDelayBeforeStart = (diff: number) => {
-    const changedResult = delayBeforeStart + diff;
-    if (changedResult < 3 || changedResult > 10) return;
-    setDelayBeforeStart(changedResult);
-  };
+  const { timeToSolveQuiz, startQuizGame, handleTimeToSolveQuiz } =
+    usePresetSetting({
+      presetPin,
+      thumbnailUrl,
+    });
 
   const redirectToLandingPage = async () => {
-    const { quizList } = await QuizRepository.getQuizByPinAsync(presetPin);
-    startQuizGame({
-      quizList,
-      thumbnailUrl: '',
-      presetPin,
-      delayBeforeStart,
-      timeToSolveQuiz: 3, // FIXME : 추후 문제 별 딜레이 설정 기능도 추가해야 함.
-    });
+    await startQuizGame();
     closeModal();
     navigate(`/quiz/loading`);
   };
@@ -54,7 +42,7 @@ const GameStartModal = ({
         완료
       </styles.StartQuizButton>
     ),
-    [delayBeforeStart],
+    [timeToSolveQuiz],
   );
 
   return (
@@ -72,9 +60,9 @@ const GameStartModal = ({
         <div>
           <styles.Title>타이머 설정</styles.Title>
           <styles.StartDelayCounter>
-            <PlusIconSvg onClick={() => handleDelayBeforeStart(1)} />
-            {delayBeforeStart}
-            <MinusIconSvg onClick={() => handleDelayBeforeStart(-1)} />
+            <PlusIconSvg onClick={() => handleTimeToSolveQuiz(1)} />
+            {timeToSolveQuiz}
+            <MinusIconSvg onClick={() => handleTimeToSolveQuiz(-1)} />
           </styles.StartDelayCounter>
         </div>
       </styles.Wrapper>
