a1a21e0fa237adc73802873b422ecab6885ffdb7
Ju-ae <qowndo@gmail.com>
2023-07-10 15:45:22 +0900

    feat: 데이터를 react-query를 사용해 가져오도록 변경

--- src/components/main/CategoryCarousel/CategoryCarousel.tsx
+++ src/components/main/CategoryCarousel/CategoryCarousel.tsx
@@ -1,33 +1,19 @@
 import { useEffect, useState } from 'react';
 
 import QuizRepository from '@/apis/quiz';
+import useGetPresetList from '@/hooks/useGetPresetList';
 import { QuizPresetType } from '@/types/quiz';
 
 import * as styles from './CategoryCarousel.style';
 import CategoryCarouselImage from './CategoryCarouselImage';
 
 const CategoryCarousel = () => {
-  const [presetList, setPresetList] = useState<QuizPresetType[]>([]);
-
-  useEffect(() => {
-    const getPresetList = async () => {
-      try {
-        const presetList = await QuizRepository.getQuizListAsync({
-          page: 1,
-          limit: 9,
-        });
-        setPresetList(presetList);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getPresetList();
-  }, []);
+  const presetList = useGetPresetList({ page: 1, limit: 9 });
 
   return (
     <styles.Wrapper>
       <styles.Carousel>
-        {presetList.map(({ presetPin, title, thumbnailUrl, hashtagList }) => (
+        {presetList && presetList.map(({ presetPin, title, thumbnailUrl, hashtagList }) => (
           <CategoryCarouselImage
             key={presetPin}
             isPrivate={false}
@@ -39,7 +25,7 @@ const CategoryCarousel = () => {
         ))}
       </styles.Carousel>
       <styles.CarouselClone>
-        {presetList.map(({ presetPin, title, thumbnailUrl, hashtagList }) => (
+        {presetList && presetList.map(({ presetPin, title, thumbnailUrl, hashtagList }) => (
           <CategoryCarouselImage
             key={presetPin}
             isPrivate={false}
--- src/hooks/useGetPresetList.ts
+++ src/hooks/useGetPresetList.ts
@@ -5,8 +5,10 @@ import QUERY_KEY from '@/constants/queryKey';
 import { PaginationType } from '@/types/quiz';
 
 const useGetPresetList = ({ page = 1, limit = 9 }: PaginationType) => {
-  const { data } = useQuery(QUERY_KEY.page({ page, limit }), () =>
-    QuizRepository.getQuizListAsync({ page, limit }),
+  const { data } = useQuery(
+    QUERY_KEY.page({ page, limit }),
+    () => QuizRepository.getQuizListAsync({ page, limit }),
+    { staleTime: 360000, cacheTime: Infinity }, //1시간이 지나면 요청 시 데이터를 가져옴.
   );
 
   return data;
