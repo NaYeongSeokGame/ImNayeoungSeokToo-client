85a649cb0c039c32e4d543bac5a9b9b2f539a24e
Bae Juae <qowndo@gmail.com>
2023-07-24 22:19:05 +0900

    [INT-44] 전체 프리셋 목록 페이지 추가 (#42)

---
+++ src/components/pages/QuizSearch/QuizSearch.style.tsx
@@ -0,0 +1,51 @@
+import { css, styled } from 'styled-components';
+
+export const SearchQuizWrapper = styled.div`
+  padding: 1rem;
+`;
+
+export const SearchForm = styled.form`
+  ${({ theme }) => {
+    return css`
+      height: 3rem;
+      padding: 0rem 1.25rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      border-radius: 0.5rem;
+      border: 0.25rem solid ${theme.colors.gray400};
+      background: ${theme.colors.white};
+    `;
+  }}
+`;
+
+export const SearchInput = styled.input`
+  ${({ theme }) => {
+    return css`
+      color: ${theme.colors.gray500};
+
+      font-family: ${theme.fonts.deco4.fontFamily};
+      font-size: ${theme.fonts.deco4.fontSize};
+      font-weight: ${theme.fonts.deco4.fontWeight};
+      line-height: normal;
+    `;
+  }}
+`;
+export const SearchButton = styled.button`
+  width: 1.125rem;
+  height: 1.125rem;
+  background: url(/src/assets/icons/PresetSearchIcon.svg);
+`;
+
+export const QuizPresetWrapper = styled.div`
+  display: flex;
+  height: 37.25rem;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 2.12rem;
+  overflow-y: auto;
+`;
+export const QuizPresetCard = styled.div`
+  margin-bottom: 1rem;
+`;
---
+++ src/components/pages/QuizSearch/QuizSearch.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+
+import PresetCard from '@/components/main/PresetCard';
+import GameStartModal from '@/components/main/GameStartModal';
+import useGetPresetList from '@/hooks/useGetPresetList';
+import useModal from '@/hooks/useModal';
+import { QuizPresetType } from '@/types/quiz';
+
+import * as styles from './QuizSearch.style';
+
+const QUIZ_COUNT_LIMIT = 6;
+
+const QuizSearch = () => {
+  const { openModal } = useModal();
+  const [page, setPage] = useState(1);
+  const observerTarget = useRef(null);
+  const presetList = useGetPresetList({ page, limit: QUIZ_COUNT_LIMIT });
+
+  //Fixme : 동작 확인 필요
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          if (presetList && QUIZ_COUNT_LIMIT > presetList.length) {
+            setPage(1);
+          } else if (page > 1) {
+            setPage(page + 1);
+          }
+        }
+      },
+      { threshold: 1 },
+    );
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
+  const handleClick = ({ presetPin, thumbnailUrl, title }: QuizPresetType) => {
+    openModal(
+      <GameStartModal
+        presetPin={presetPin}
+        thumbnailUrl={thumbnailUrl}
+        title={title}
+      />,
+    );
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <styles.SearchQuizWrapper>
+      <styles.SearchForm onSubmit={handleSubmit}>
+        <styles.SearchInput placeholder="#태그를 검색하세요" />
+        <styles.SearchButton />
+      </styles.SearchForm>
+      <styles.QuizPresetWrapper>
+        {presetList &&
+          presetList.map((preset) => (
+            <styles.QuizPresetCard >
+              <PresetCard
+              key={preset.presetPin}
+                title={preset.title}
+                thumbnailUrl={preset.thumbnailUrl}
+                hashtagList={preset.hashtagList}
+                handleClick={() => handleClick(preset)}
+              />
+            </styles.QuizPresetCard>
+          ))}
+      </styles.QuizPresetWrapper>
+      <div ref={observerTarget}></div>
+    </styles.SearchQuizWrapper>
+  );
+};
+
+export default QuizSearch;
