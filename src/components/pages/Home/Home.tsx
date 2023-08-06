d07f0444d950bf9663b106bf8385e7445d465512
uaej <qowndo@gmail.com>
2023-08-07 00:56:05 +0900

    fix: 버튼의 여백을 클릭하면 동작하지 않는 문제 해결

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import CategoryCarousel from '@/components/main/CategoryCarousel';
 import JoinPresetModal from '@/components/main/JoinPresetModal';
@@ -8,20 +8,24 @@ import * as styles from './Home.style';
 
 const Home = () => {
   const { openModal } = useModal();
+  const navigate = useNavigate();
 
   const openJoinPresetModal = () => openModal(<JoinPresetModal />);
+  const openCreateQuiz = () => navigate('/create');
+  const openSearchQuiz = () => navigate('/search');
+
   return (
     <>
       <CategoryCarousel />
       <styles.ButtonSection>
-        <styles.OptionButton>
-        <Link to="/create">직접 퀴즈 만들기</Link>
+        <styles.OptionButton onClick={openCreateQuiz}>
+          직접 퀴즈 만들기
         </styles.OptionButton>
         <styles.OptionButton onClick={openJoinPresetModal}>
           비공개 PIN 입력하기
         </styles.OptionButton>
-        <styles.OptionButton>
-          <Link to="/search">퀴즈목록 보러가기</Link>
+        <styles.OptionButton onClick={openSearchQuiz}>
+          퀴즈목록 보러가기
         </styles.OptionButton>
       </styles.ButtonSection>
       <styles.Title>나도... 나영석!</styles.Title>
